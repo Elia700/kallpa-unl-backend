376303b148ae6452783c828408d3c28ecc2ece65
Elia700 <elias.poma@unl.edu.ec>
2026-02-05 15:52:45 -0500

    Add health endpoint

--- index.py
+++ index.py
@@ -9,11 +9,7 @@ def home():
 
 @app.route("/health")
 def health():
-    return jsonify({
-        "status": "OK",
-        "message": "Kallpa Backend is running",
-        "timestamp": "2026-02-05"
-    }), 200
+    return "OK", 200
 
 if __name__ == "__main__":
    app.run(port=5000)
